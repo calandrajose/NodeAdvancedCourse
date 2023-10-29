b0678c85d097b3d065985c2d9cecc11ed920176e
jose ignacio calandra <calandrajosei@gmail.com>
2023-10-29 20:04:05 -0300

    add clustering

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster')
 
-app.get('/', (req, res) => {
-    res.send('Hi there');
-})
+if(cluster.isMaster){
+    cluster.fork();
+    // cluster.fork();
+} else {
 
-app.listen(3000);
+    const crypto = require('crypto'); 
+    const express = require('express')
+    const app = express();
+
+    app.get('/', (req, res) => {
+
+        crypto.pbkdf2('a', 'b', 100000, 1000, 'sha512', () => {
+            res.send('Hi there');
+        })
+    })
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast');
+    })
+
+    app.listen(3000);
+}
