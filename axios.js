69e78d28e859981d6f182b527370e1f803f6406a
jose ignacio calandra <calandrajosei@gmail.com>
2023-10-29 22:03:24 -0300

    delete data

--- axios.js
+++ axios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function call(){
     return axios.
-    get(`https://chedrauimx.vtexcommercestable.com.br/api/catalog_system/pub/specification/fieldGet/359`)
+    get(`https://{clientAccount}.vtexcommercestable.com.br/api/catalog_system/pub/specification/fieldGet/359`)
     .then(({status, statusText, data}) => {
         console.log(status, statusText, data)
     })
@@ -13,7 +13,7 @@ console.log(res)
 
 
 // async function call(){
-//     return axios.get(`https://chedrauimx.vtexcommercestable.com.br/api/catalog_system/pub/specification/fieldGet/359`);
+//     return axios.get(`https://{clientAccount}.vtexcommercestable.com.br/api/catalog_system/pub/specification/fieldGet/359`);
 // }
 // 
 // const res = call()
